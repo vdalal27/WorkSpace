2e1b33b4751c8ad13e62e9a27ddf88c940fa41f8
Vandana Dalal <Vanu@Vandanas-MacBook-Pro.local>
2019-04-15 22:12:31 -0700

    basic form and a bit styling

--- Documents/jsx/src/index.js
+++ Documents/jsx/src/index.js
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom';
 
 //create react component
 const App = function(){
-    return <div>
-        Hello, from Shinigami!!!
-    </div>;
+    return(
+    <div>
+        <label class="label" for="name">Name:</label>
+        <input id="name" type="text"/>
+        <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+    </div>
+    ); 
 };
 //display
 ReactDOM.render(<App />,document.querySelector('#root'));
