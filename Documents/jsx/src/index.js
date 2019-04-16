95462f4c4d1d182bc390b19bbb58b37833fecd23
Vandana Dalal <Vanu@Vandanas-MacBook-Pro.local>
2019-04-16 08:07:15 -0700

    function for button

--- Documents/jsx/src/index.js
+++ Documents/jsx/src/index.js
@@ -2,13 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//function for button
+function clickMe(){
+    return "Click Me!";
+}
+
 //create react component
 const App = function(){
     return(
     <div>
-        <label class="label" for="name">Name:</label>
+        <label className="label" for="name">Name:</label>
         <input id="name" type="text"/>
-        <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+        <button style={{ backgroundColor: 'blue', color: 'white' }}>{clickMe()}</button>
     </div>
     ); 
 };
