1d03556e161d563e7f8eb591adb3301d90a2da46
Vandana Dalal <Vanu@Vandanas-MacBook-Pro.local>
2019-04-16 22:45:42 -0700

    react components

---
+++ Documents/react-projects/components/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function(){
+    return <div>Hi there!</div>
+};
---
+++ Documents/react-projects/components/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function(){
+    return <div>Hi there!</div>
+};
+
+ReactDOM.render(
+    <App />, document.querySelector("#root")
+);
