816cac1f9e8a7751568b3f87a05ee47cb26fc4dd
Vandana Dalal <Vanu@Vandanas-MacBook-Pro.local>
2019-07-25 08:51:34 -0700

    Player with triple shooting abilities added

---
+++ Documents/Game-Development/Space Shooter/Assets/Game/Space-Shooter-Scripts/Scripts/Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//based on udemy course "The Ultimate Guide to Game Development" by Jonathan Weinberger
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        //destroy laser object at 5.45f
+        if(transform.position.y>=5.45f){
+            Destroy(this.gameObject);
+        }
+    }
+}
